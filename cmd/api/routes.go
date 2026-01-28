75b1e662ad87e856b4b33ed8244aafc39b941e81
moges7624 <moges7624@gmail.com>
2026-01-28 05:32:38 +0300

    lib: setting up httprouter

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,6 +10,8 @@ func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthCheckHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 
 	return router
 }
