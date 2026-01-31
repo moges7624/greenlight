69f415d236f584830b0dae06072713e9f897af94
moges <moges7624@gmail.com>
2026-01-31 16:24:17 +0300

    ch6: sql migrations

---
+++ migrations/000002_add_movies_check_constraints.up.sql
@@ -0,0 +1,7 @@
+ALTER TABLE movies ADD CONSTRAINT movies_runtime_check CHECK (runtime >= 0);
+ALTER TABLE movies ADD CONSTRAINT movies_year_check CHECK (
+year BETWEEN 18888 AND date_part('year', now())
+);
+ALTER TABLE movies ADD CONSTRAINT genres_length_check CHECK (
+array_length(genres, 1) BETWEEN 1 AND 5
+);
