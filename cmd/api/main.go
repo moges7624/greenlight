5b9593e7bb860390824cef42dc6c90835f78df42
moges <moges7624@gmail.com>
2026-02-04 16:58:47 +0300

    ch14: sending email
    - sending email using smtp
    - sending emails in the background
    - graceful shutdown of background tasks

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,11 +5,13 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"sync"
 	"time"
 
 	_ "github.com/lib/pq"
 	"github.com/moges7624/greenlight/internal/data"
 	"github.com/moges7624/greenlight/internal/jsonlog"
+	"github.com/moges7624/greenlight/internal/mailer"
 )
 
 const version = "1.0.0"
@@ -28,12 +30,21 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *jsonlog.Logger
 	models data.Models
+	mailer mailer.Mailer
+	wg     sync.WaitGroup
 }
 
 func main() {
@@ -78,6 +89,15 @@ func main() {
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 25, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username",
+		"a3570ff3f6df0c", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password",
+		"6b654b8275d753", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender",
+		"Greenlight <no-reply@greenlight.moges.net>", "SMTP sender")
+
 	flag.Parse()
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
@@ -94,6 +114,13 @@ func main() {
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer.New(
+			cfg.smtp.host,
+			cfg.smtp.port,
+			cfg.smtp.username,
+			cfg.smtp.password,
+			cfg.smtp.sender,
+		),
 	}
 
 	err = app.serve()
