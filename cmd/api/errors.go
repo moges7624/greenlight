ac2c9d896d4c21d875f6e3299de6bcd96d7c53db
moges <moges7624@gmail.com>
2026-02-05 20:28:27 +0300

    ch16: athentication

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -85,3 +85,21 @@ func (app *application) rateLimitExceededResponse(
 	message := "rate limit exceeded"
 	app.errorResponse(w, r, http.StatusTooManyRequests, message)
 }
+
+func (app *application) invalidCredentialsResponse(
+	w http.ResponseWriter,
+	r *http.Request,
+) {
+	message := "invalid authentication credentials"
+	app.errorResponse(w, r, http.StatusUnauthorized, message)
+}
+
+func (app *application) invalidAuthenticationTokenResponse(
+	w http.ResponseWriter,
+	r *http.Request,
+) {
+	w.Header().Set("WWW-Authenticate", "Bearer")
+
+	message := "invalid or missing authentication token"
+	app.errorResponse(w, r, http.StatusUnauthorized, message)
+}
